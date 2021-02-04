be6fa1cd05dd1b6009466c38bfed6a6caa47e7ab
Daniel <xdcargil021@ieshnosmachado.org>
2021-02-04 16:26:38 +0100

    Creado Cliente

---
+++ js/cliente.js
@@ -0,0 +1,49 @@
+"use strict";
+
+class Cliente {
+  // Constructor de atributos
+  constructor(nombre, apellidos, DNI) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.DNI = DNI;
+  }
+
+
+
+  //Getters y Setters
+  getNombre(){
+    return this.nombre;
+  }
+  setNombre(nombre){
+    this.nombre = nombre;
+  }
+
+
+  getApellidos(){
+    return this.apellidos;
+  }
+  setApellidos(apellidos){
+    this.apellidos = apellidos;
+  }
+
+
+  getDNI(){
+    return this.DNI;
+  }
+  setDNI(DNI){
+    this.DNI = DNI;
+  }
+
+
+  
+  // Métodos
+  toHTMLRow() {
+    let sFila = "<tr>";
+    sFila += "<td>" + this.nombre + "</td>";
+    sFila += "<td>" + this.apellidos + "</td>";
+    sFila += "<td>" + this.DNI + "</td>";
+    sFila += "</tr>";
+
+    return sFila;
+  }
+}
