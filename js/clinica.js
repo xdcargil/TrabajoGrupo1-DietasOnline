03f001b5514840838b50b8a6bf10e878167d41c1
jcrufra875 <61141149+jcrufra875@users.noreply.github.com>
2021-02-05 11:14:39 +0100

    Update clinica.js

---
+++ js/clinica.js
@@ -0,0 +1,44 @@
+'use strict';
+
+class Clinica
+{
+    constructor(nombre_clinica)
+    {
+        this.nombre_clinica=nombre_clinica;
+        this.dietistas=[];
+        this.cliente=[];
+        this.dietas=[];
+    }
+    listarClientes() //Lista todos los clientes
+    {
+
+    }
+    listaClienteEspecifico(oCliente) //Se pasa un valor String (DNI del Cliente)
+    {
+
+    }
+    listarDietistas() //Listar todos los dietistas
+    {
+
+    }
+    listarDietista(oDietista) //Se pasa un valor String (DNI del Dietista)
+    {
+
+    }
+    listarDietas() //Listar todas las dietas
+    {
+
+    }
+    listarDietaEspecifica(oDieta) //Se pasa un valor INT (Código de dieta)
+    {
+
+    }
+    altaCliente(oCliente) //Se pasa un objeto cliente
+    {
+
+    }
+    bajaCliente(oCliente) //Se pasa un objeto cliente
+    {
+
+    }
+}
