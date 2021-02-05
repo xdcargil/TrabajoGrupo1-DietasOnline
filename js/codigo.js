0ae108c02bc36724e941862905c50a4d61585495
Miguel-Guerrero13 <73499244+Miguel-Guerrero13@users.noreply.github.com>
2021-02-05 17:45:04 +0100

    Update codigo.js
    Codigo.js

---
+++ js/codigo.js
@@ -0,0 +1,59 @@
+"use strict";
+//Creamos el objeto tienda para añadirle datos posteriormente
+let clinica = new clinica();
+
+/*-------------------------EVENTSLISTENERS----------------------------------------------*/
+
+
+//Llamada a la Función de Ocultar Formularios.
+ocultarFormularios();
+
+
+//Mostrar los formularios
+
+
+//Función para Ocultar los Formularios
+function ocultarFormularios() {
+}
+
+
+function limpiarInputs(inputs) {
+
+    for (let index = 0; index < inputs.length; index++) {
+        if (inputs[index].type != "button") {
+            inputs[index].value = "";
+        }
+    }
+}
+
+
+
+
+//Registrar Cliente
+
+function altaCliente() {
+
+    /*let formulario = document.getElementById("formAdministracionUsuario");
+    let inputs = form.getElementsByTagName("input");
+
+    let sNombre = inputs[0].value;
+    let sApellidos = inputs[1].value;
+    let dFecha = inputs[2].value;
+    let sEmail = inputs[3].value;
+    iContadorCliente++;
+*/
+    let cliente = new Cliente(iContadorCliente, sNombre, sApellidos, dFecha, sEmail);
+
+    if(tienda.registrarCliente(usuario)){
+        alert("Cliente dado de alta correctamente");
+        ocultarFormularios();
+    }else{
+        alert("Ya existe un cliente con ese NI");
+    }
+
+    limpiarInputs(inputs);
+
+
+}
+
+
